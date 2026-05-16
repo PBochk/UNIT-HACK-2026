11f41a7907b8eb41ea26d545aa4e2755a055af21
v_yashchik <medyantsevsvyt@gmail.com>
2026-05-16 10:56:17 +0500

    fix flippers fixed

--- Assets/Scripts/Obstacles/BumperController.cs
+++ Assets/Scripts/Obstacles/BumperController.cs
@@ -2,14 +2,14 @@
 
 public sealed class BumperController : MonoBehaviour
 {
-    [SerializeField] private float _bounceForce = 15f;
+    [SerializeField] private float bounceForce = 15f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
         Vector2 direction = collision.transform.position - transform.position;
         rb.linearVelocity = Vector2.zero;
-        rb.AddForce(direction.normalized * _bounceForce, ForceMode2D.Impulse);
+        rb.AddForce(direction.normalized * bounceForce, ForceMode2D.Impulse);
         
     }
 }
