01c324923fefbfe72f1c94902c902fe7b01e64d8
v_yashchik <medyantsevsvyt@gmail.com>
2026-05-16 11:21:13 +0500

    feat: scores increemnting implemnted

---
+++ Assets/Scripts/Obstacles/ScorableTarget.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public sealed class ScorableTarget : MonoBehaviour
+{
+    [SerializeField] private Currency scoreValue;
+    [SerializeField] private float cooldownDuration = 2f;
+    [SerializeField] private int scores;
+
+    public event Action<Currency> OnScoreAwarded;
+
+    private bool _canTrigger = true;
+    private Color _activeColor;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ball")) return;
+        if (_canTrigger)
+        {
+            TriggerTarget();
+        }
+    }
+
+    private void TriggerTarget()
+    {
+        _canTrigger = false;
+        scoreValue.Amount +=  scores;
+        Debug.Log(scoreValue.Amount);
+        OnScoreAwarded?.Invoke(scoreValue);
+
+        StartCoroutine(CooldownRoutine());
+    }
+
+    private IEnumerator CooldownRoutine()
+    {
+        yield return new WaitForSeconds(cooldownDuration);
+        
+        _canTrigger = true;
+    }
+}
