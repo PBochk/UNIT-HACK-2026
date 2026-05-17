6cfac746efbf13de3c811f6db99060819b8e9f62
PBochk <pbochkarev06@gmail.com>
2026-05-17 17:14:12 +0500

    fix: for first build

--- Assets/Scripts/GlobalData/Upgrade.cs
+++ Assets/Scripts/GlobalData/Upgrade.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
-using Mono.Cecil.Cil;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Upgrade", menuName = "Scriptable Objects/Upgrade")]
